3718041fcb16690ee41ab6e3b965bfc07a591261
theshaydays <ahmedshayer6@gmail.com>
2022-05-28 03:16:07 +0800

    conduct fft
    conducted fft on an audio sample. It works!

--- whistle/lib/fftAnalysis.dart
+++ whistle/lib/fftAnalysis.dart
@@ -1,8 +1,9 @@
+import 'dart:typed_data';
 import 'package:fftea/fftea.dart';
 import 'package:flutter/services.dart' show rootBundle;
-import 'dart:io';
 import 'package:wav/wav.dart';
 import 'package:file_picker/file_picker.dart';
+import 'dart:math';
 
 class fftAnalysis {
   Future<String> loadAudio() async {
@@ -10,17 +11,47 @@ class fftAnalysis {
   }
 
   void main() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles();
-    File file;
+    FilePickerResult? result =
+        await FilePicker.platform.pickFiles(type: FileType.audio);
+    String filePath = '';
     if (result != null) {
-      File file = File(result.files.single.path);
-    } else {}
+      filePath = result.files.single.path as String;
+    } else {
+      // user cancelled picker
+    }
 
-    //final audio = await Wav.readFile('whistle/assets/audio/note1.wav');
-    List<double> myData = file.readAsBytesSync();
-    final fft = FFT(myData.length);
-    final freq = fft.realFft(myData);
-    print(freq);
+    final sound = await Wav.readFile(filePath);
+    List<double> audio = sound.toMono();
+
+    final chunkSize = 2048;
+    final stft = STFT(chunkSize, Window.hanning(chunkSize));
+
+    final spectrogram = <Float64List>[];
+    stft.run(audio, (Float64x2List freq) {
+      spectrogram.add(freq.discardConjugates().magnitudes());
+
+      List<double> list = freq.discardConjugates().magnitudes().toList();
+      double maxVal = 0;
+      int idx = 0;
+      for (int i = 0; i < list.length; i++) {
+        if (list[i] > maxVal) {
+          maxVal = list[i];
+          idx = i;
+        }
+      }
+      print(idx);
+      print(freq
+          .discardConjugates()
+          .magnitudes()
+          .toList()
+          .reduce(max)
+          .toString());
+    });
+    print('value is ' + stft.frequency(49, 44100).toString());
+
+    // final fft = FFT(myData.length);
+    // final freq = fft.realFft(myData);
+    // print(freq);
 
     //String filePath = fftAnalysis().loadAudio();
     //final wav = await Wav.readFile(filePath);
