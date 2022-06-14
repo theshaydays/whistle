b85f64806f480e27652f76476b0ba7a187966431
jodytng <tngjody@gmail.com>
2022-06-14 16:26:41 +0800

    dictionary of notes

--- whistle/lib/RecentProjects.dart
+++ whistle/lib/RecentProjects.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:whistle/models/constants.dart';
+import 'package:piano/piano.dart';
 
 class RecentProjects extends StatefulWidget {
   @override
@@ -7,59 +7,25 @@ class RecentProjects extends StatefulWidget {
 }
 
 class _RecentProjectsState extends State<RecentProjects> {
-  Future<bool?> showWarning(BuildContext context) async => showDialog<bool>(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('Do you want to return to home page?'),
-          content: Text('Changes made on this page will not be saved'),
-          actions: [
-            ElevatedButton(
-              child: Text('No'),
-              style: ElevatedButton.styleFrom(
-                primary: kPrimaryColor,
-              ),
-              onPressed: () => Navigator.pop(context, false),
-            ),
-            ElevatedButton(
-              child: Text('Yes'),
-              style: ElevatedButton.styleFrom(
-                primary: kPrimaryColor,
-              ),
-              onPressed: () => Navigator.pop(context, true),
-            ),
-          ],
-        ),
-      );
-
-  @override
-  Widget build(BuildContext context) => WillPopScope(
-        onWillPop: () async {
-          final shouldPop = await showWarning(context);
-          return shouldPop ?? false;
-        },
-        child: Scaffold(
-          appBar: AppBar(
-            leading: BackButton(),
-            backgroundColor: kPrimaryColor,
-            title: Text(
-              'Score Sheet',
-              style: TextStyle(
-                  fontSize: 15.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w800),
-            ),
-            centerTitle: true,
-            actions: [
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Icon(
-                  Icons.more_horiz,
-                  color: Colors.white,
-                  size: 30,
-                ),
-              )
-            ],
-          ),
-        ),
-      );
+  Widget build(BuildContext context) {
+    return ClefImage(
+      clef: Clef.Treble,
+      noteRange: NoteRange(NotePosition(note: Note.C, octave: -10),
+          NotePosition(note: Note.C, octave: 10)),
+      noteImages: [
+        NoteImage(
+            notePosition: NotePosition(
+                note: Note.B, octave: 3, accidental: Accidental.Sharp),
+            offset: 0.25),
+        NoteImage(
+          notePosition: NotePosition(
+              note: Note.E, octave: 5, accidental: Accidental.Flat),
+          offset: 0.25,
+        )
+      ],
+      clefColor: Colors.white,
+      noteColor: Colors.white,
+      size: Size.zero,
+    );
+  }
 }
---
+++ whistle/lib/models/Notes.dart
@@ -0,0 +1,4 @@
+void main() {
+  var notes = {'G,6,0.5': 'tom', 'Password': 'pass@123'};
+  print(notes);
+}
