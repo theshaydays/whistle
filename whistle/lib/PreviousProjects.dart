d14417abb5b9e734709fd4632f2aa0e950a32554
jodytng <tngjody@gmail.com>
2022-06-14 13:40:13 +0800

    updated PreviousProjects.dart

--- whistle/lib/PreviousProjects.dart
+++ whistle/lib/PreviousProjects.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: import_of_legacy_library_into_null_safe
 
 import 'package:flutter/material.dart';
+import 'package:piano/piano.dart';
 // import 'package:sheet_music/sheet_music.dart';
 
 class PreviousProjects extends StatefulWidget {
@@ -10,12 +11,21 @@ class PreviousProjects extends StatefulWidget {
 
 class _PreviousProjectsState extends State<PreviousProjects> {
   Widget build(BuildContext context) {
-    return MaterialApp(
-        // home: SheetMusic(
-        //   scale: "C Major",
-        //   pitch: "C4",
-        //   trebleClef: true,
-        // ),
-        );
+    return ClefImage(
+      clef: Clef.Treble,
+      noteRange: NoteRange(NotePosition(note: Note.C, octave: -10),
+          NotePosition(note: Note.C, octave: 10)),
+      noteImages: [
+        NoteImage(
+            notePosition: NotePosition(note: Note.B, octave: 3), offset: 0.1),
+        NoteImage(
+          notePosition: NotePosition(note: Note.E, octave: 5),
+          offset: 0.25,
+        )
+      ],
+      clefColor: Colors.white,
+      noteColor: Colors.white,
+      size: Size.zero,
+    );
   }
 }
