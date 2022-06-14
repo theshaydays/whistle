e6b1c91cb12c1fdd56855fcd6f9f18ca7aea9a46
jodytng <tngjody@gmail.com>
2022-06-14 15:52:33 +0800

    tried to fix podn install problem

--- whistle/lib/PreviousProjects.dart
+++ whistle/lib/PreviousProjects.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:piano/piano.dart';
-// import 'package:sheet_music/sheet_music.dart';
+import 'package:whistle/models/constants.dart';
 
 class PreviousProjects extends StatefulWidget {
   @override
@@ -10,22 +10,95 @@ class PreviousProjects extends StatefulWidget {
 }
 
 class _PreviousProjectsState extends State<PreviousProjects> {
-  Widget build(BuildContext context) {
-    return ClefImage(
-      clef: Clef.Treble,
-      noteRange: NoteRange(NotePosition(note: Note.C, octave: -10),
-          NotePosition(note: Note.C, octave: 10)),
-      noteImages: [
-        NoteImage(
-            notePosition: NotePosition(note: Note.B, octave: 3), offset: 0.1),
-        NoteImage(
-          notePosition: NotePosition(note: Note.E, octave: 5),
-          offset: 0.25,
-        )
-      ],
-      clefColor: Colors.white,
-      noteColor: Colors.white,
-      size: Size.zero,
-    );
-  }
+  Future<bool?> showWarning(BuildContext context) async => showDialog<bool>(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Do you want to return to home page?'),
+          content: Text('Changes made on this page will not be saved'),
+          actions: [
+            ElevatedButton(
+              child: Text('No'),
+              style: ElevatedButton.styleFrom(
+                primary: kPrimaryColor,
+              ),
+              onPressed: () => Navigator.pop(context, false),
+            ),
+            ElevatedButton(
+              child: Text('Yes'),
+              style: ElevatedButton.styleFrom(
+                primary: kPrimaryColor,
+              ),
+              onPressed: () => Navigator.pop(context, true),
+            ),
+          ],
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) => WillPopScope(
+        onWillPop: () async {
+          final shouldPop = await showWarning(context);
+          return shouldPop ?? false;
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            leading: BackButton(),
+            backgroundColor: kPrimaryColor,
+            title: Text(
+              'Score Sheet',
+              style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w800),
+            ),
+            centerTitle: true,
+            actions: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Icon(
+                  Icons.more_horiz,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              )
+            ],
+          ),
+          body: ClefImage(
+            clef: Clef.Treble,
+            size: Size(10.0, 10.0),
+            noteRange: NoteRange(NotePosition(note: Note.C, octave: -10),
+                NotePosition(note: Note.C, octave: 10)),
+            noteImages: [
+              NoteImage(
+                  notePosition: NotePosition(note: Note.B, octave: 3),
+                  offset: 0.1),
+              NoteImage(
+                notePosition: NotePosition(note: Note.E, octave: 5),
+                offset: 0.25,
+              )
+            ],
+            clefColor: kPrimaryColor,
+            noteColor: kPrimaryColor,
+          ),
+        ),
+      );
 }
+
+/*Widget _buildScoreSheet(BuildContext context) {
+  return ClefImage(
+    clef: Clef.Treble,
+    noteRange: NoteRange(NotePosition(note: Note.C, octave: -10),
+        NotePosition(note: Note.C, octave: 10)),
+    noteImages: [
+      NoteImage(
+          notePosition: NotePosition(note: Note.B, octave: 3), offset: 0.1),
+      NoteImage(
+        notePosition: NotePosition(note: Note.E, octave: 5),
+        offset: 0.25,
+      )
+    ],
+    clefColor: kPrimaryColor,
+    noteColor: kPrimaryColor,
+    size: Size.zero,
+  );
+}*/
