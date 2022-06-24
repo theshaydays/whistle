e2a0f04d8c352672a2108c64c65270a4db83412e
theshaydays <ahmedshayer6@gmail.com>
2022-06-25 04:23:40 +0800

    add in some imports

--- whistle/lib/FFmpegConvert.dart
+++ whistle/lib/FFmpegConvert.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'dart:io';
 
 import 'package:ffmpeg_kit_flutter/ffprobe_kit.dart';
--- whistle/test/unit_test.dart
+++ whistle/test/unit_test.dart
@@ -1,17 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:whistle/FFmpegConvert.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   test('Audio conversion', () async {
     //setup
-    FFmpegConvert ffmpegInstance = FFmpegConvert(
-        'C:\\Users\\shayer\\Documents\\NUS\\Orbital\\royalty.mp3');
+    FFmpegConvert ffmpegInstance =
+        FFmpegConvert('C:/Users/shayer/Documents/NUS/Orbital/royalty.mp3');
     String output = '';
 
     //do
     output = await ffmpegInstance.convertFile();
 
     //test
-    expect(output, 'C:\\Users\\shayer\\Documents\\NUS\\Orbital\\royalty.wav');
+    expect(output, 'C:/Users/shayer/Documents/NUS/Orbital/royalty.wav');
   });
 }
