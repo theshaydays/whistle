2af38f6411190eea92787dced63a099b988ed5d1
theshaydays <ahmedshayer6@gmail.com>
2022-07-20 19:39:42 +0800

    Revert "more firebase stuff"
    This reverts commit d0e5829e16c3c6b71baa503470574145658d4390.

--- whistle/macos/Flutter/GeneratedPluginRegistrant.swift
+++ whistle/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import ffmpeg_kit_flutter
 import firebase_analytics
 import firebase_auth
 import firebase_core
-import firebase_database
 import just_audio
 import path_provider_macos
 
@@ -20,7 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
