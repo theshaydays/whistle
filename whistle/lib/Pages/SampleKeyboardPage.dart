ede7e8f4296016d918ef6c7deb718f720722fe96
theshaydays <ahmedshayer6@gmail.com>
2022-07-25 03:09:54 +0800

    sample keyboard actually works now

--- whistle/lib/Pages/SampleKeyboardPage.dart
+++ whistle/lib/Pages/SampleKeyboardPage.dart
@@ -1,19 +1,39 @@
 // ignore_for_file: import_of_legacy_library_into_null_safe
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:piano/piano.dart';
-import '../main.dart';
+import 'package:whistle/models/Notes.dart';
 
 class SampleKeyboardPage extends StatefulWidget {
   @override
   _SampleKeyboardPageState createState() => _SampleKeyboardPageState();
 }
 
-void main() {
-  runApp(MyApp());
-}
-
 class _SampleKeyboardPageState extends State<SampleKeyboardPage> {
+  AudioPlayer _audioPlayer = AudioPlayer();
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _audioPlayer.setAsset('audio/Piano_C4.mp3');
+  // }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      // Release the player's resources when not in use. We use "stop" so that
+      // if the app resumes later, it will still remember what position to
+      // resume from.
+      _audioPlayer.stop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
@@ -30,6 +50,16 @@ class _SampleKeyboardPageState extends State<SampleKeyboardPage> {
             Clef.Treble,
           ]),
           onNotePositionTapped: (position) {
+            String note = keyboard[position.name] as String;
+            print(note);
+            if (_audioPlayer.playing) {
+              AudioPlayer tempPlayer = AudioPlayer();
+              tempPlayer.setAsset('audio/Piano_$note.mp3');
+              tempPlayer.play();
+            } else {
+              _audioPlayer.setAsset('audio/Piano_$note.mp3');
+              _audioPlayer.play();
+            }
             // Use an audio library like flutter_midi to play the sound
           },
         ),
