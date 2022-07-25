49fcb6e715b1cc1ea5aa61d4dcfc671b5d01d5f2
theshaydays <ahmedshayer6@gmail.com>
2022-07-25 19:22:32 +0800

    added ignore

--- whistle/lib/AlgorithmMethods/FFTAnalysis.dart
+++ whistle/lib/AlgorithmMethods/FFTAnalysis.dart
@@ -83,6 +83,7 @@ class FFTAnalysis {
       }
 
       //print(list[idx]);
+      // ignore: todo
       //TODO: determine whether should be <= or <, <= excludes first sound sample, < includes first sound sample
       if (list[idx] <= volThreshold!) {
         loudEnough = false;
